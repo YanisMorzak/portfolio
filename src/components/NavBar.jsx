3fd4b423e78fa2ddd12b086c689bd78008503684
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-18 11:56:54 +0100

    💄   F01-Header: Add NavBar style

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -18,17 +18,36 @@ export default function NavBar() {
 
 const NavBarStyled = styled.div`
   height: 100px;
-  padding: 10px;
+  padding: 10px 5% 10px 20px;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
   border: 1px solid blue;
   .logo {
     height: 90%;
   }
   .list {
-    flex: 1;
     border: 1px solid orange;
     list-style: none;
-    display: flex;
-    justify-content: space-around;
+
+    li {
+      display: inline-block;
+      margin: 10px 20px;
+      position: relative;
+
+      &::after {
+        content: "";
+        height: 1px;
+        width: 0;
+        background: #3d4143;
+        position: absolute;
+        bottom: -6px;
+        left: 0;
+        transition: 0.5s;
+      }
+      &:hover::after {
+        width: 100%;
+      }
+    }
   }
 `;
