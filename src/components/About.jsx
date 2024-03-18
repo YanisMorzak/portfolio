fefc5568de767b0971cbb3e27f2ab07ae44716b2
nom utilisateur de votre repo git <morzak78@gmail.com>
2024-03-18 16:18:55 +0100

    🚧  F01-Header: uninstall routing because the portfolio will fit on one page (and add About)

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+export default function About() {
+  return (
+    <AboutStyled>
+      <div id="about">About</div>
+    </AboutStyled>
+  );
+}
+
+const AboutStyled = styled.div`
+  height: 100vh;
+`;
